c90c50efba62e24f36ab9fe06f48a0a6d8630d80
ehkere52 <supercruhs33@gmail.com>
2025-11-03 12:09:45 +0300

    Car.cs

---
+++ Car.cs
@@ -0,0 +1,35 @@
+namespace 11praktik
+{
+    internal class Car
+    {
+        private int speed;
+
+        public int Speed
+        {
+            get { return speed; }
+            set
+            {
+                if (value < 0 || value > 300)
+                {
+                    speed = 150; //default value
+                    Console.WriteLine("Ошибка, некоректное значение!");
+                }
+                else
+                { 
+                    speed = value;                
+                }
+            }
+        }
+        public string Model { get; set; }
+        public Car(int speed, string model) 
+        {
+            Speed = speed;
+            Model = model;
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine($"{Model} едет со скоростью {speed} км/ч.");
+        }
+    }
+}
